c84ff4f31cd0c3ad8f181dc4a93d3d1395ba70ca
Shadaaaw <shadaaaw@gmail.com>
2026-07-10 17:33:28 +0100

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -13,7 +13,11 @@ const API_BASE_URL = (() => {
     );
   }
 
-  return 'http://localhost:4000/api';
+  if (!process.env.NEXT_PUBLIC_API_URL) {
+  throw new Error("NEXT_PUBLIC_API_URL is missing");
+}
+
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 })();
 
 const getAuthToken = () => {
